af3b49c2800b50bb079e77a0d765ae9a31aa9305
Diego Cabrera <diego362a@gmail.com>
2021-12-07 00:12:15 -0500

    'cambios'

--- src/Components/Encuesta.js
+++ src/Components/Encuesta.js
@@ -7,7 +7,7 @@ import URL_SERVIDOR from "../constantes";
 export const Encuesta = () => {
 
     function getEncuestas() {
-        return axios.get(URL_SERVIDOR +"/encuestas",{
+        return axios.get(URL_SERVIDOR+"/encuestas",{
           headers:{
             Authorization : "Bearer " + localStorage.getItem('token')
           }
